8e5795668159a871ff055d42832a7f51bafe87b0
adis omurov <adisomurov003@gmail.com>
2021-05-24 16:54:37 +0600

    rename repos

--- src/component/AntBuilder/AntControls/AntControl/AntControl.js
+++ src/component/AntBuilder/AntControls/AntControl/AntControl.js
@@ -4,7 +4,9 @@
 // import { useDispatch } from "react-redux";
 // import { add, remove } from "../../../../store/action/builder";
 
+import classes from "./AntControl.module.css";
 import Button from "../../../UI/Button/Button";
+import AntIngredient from "../../../AntIngredient/AntIngredient";
 
 const AntControl = ({ type,  count }) => {
     const dispatch = useDispatch()
